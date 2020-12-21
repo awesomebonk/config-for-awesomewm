c672404f3ef6fbc738bdf672e19a41d82740b5d9
awesomebonk <76153736+awesomebonk@users.noreply.github.com>
2020-12-21 15:25:34 +0300

    Add installation script's for fedora and debian scripts

---
+++ install_deb_based.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+echo "Installing dependencies for debian based distros"
+sudo apt install awesome kitty compton geany maim frei0r-plugins mate-power-manager nemo dmenu
---
+++ install_fedora.sh
@@ -0,0 +1,5 @@
+# !/bin/bash
+
+echo "Installing dependencies"
+sudo dnf install awesome kitty compton geany xfce4-terminal maim frei0r-plugins dmenu nemo mate-polkit
+
